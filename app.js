476e3abb18977362e80c5f73b2edfbd42ee2275e
SBhattacharya45 <umyadeepb27@gmail.com>
2020-08-14 12:55:54 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,111 @@
+var express = require('express');
+var app = express();
+const bodyParser = require('body-parser');
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+const { v4: uuidv4 } = require('uuid');
+require('dotenv').config();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.json({limit: '100mb'}));
+app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
+
+var debug_log = require('debug')('whiteboard:log');
+var debug_error = require('debug')('whiteboard:error');
+
+// set static folder
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+})
+
+app.get('/:boardId', function (req, res) {
+	res.render('index', {boardId: req.params.boardId});
+});
+
+
+// app.post('/save', function (req, res) {
+//     fs.writeFile("./server/data.txt", req.body.data, function(err) {
+//         if(err) {
+//             return console.log(err);
+//         }
+//         console.log("The file was saved!");
+//     });
+
+//     res.status(200).json({
+//         message: "Stored successfully"
+//     })
+// });
+
+// app.get('/load', function (req, res) {
+//     fs.readFile('./server/data.txt','utf8', function (err, data) {
+//         if (err) {
+//             return console.log(err)
+//         }
+//         const content = data;
+//         res.status(200).json({
+//             data: content
+//         })
+//     });
+// });
+
+// app.post('/saveHeight', function (req, res) {
+//     fs.writeFile("./server/height.txt", req.body.data, function(err) {
+//         if(err) {
+//             return console.log(err);
+//         }
+//         console.log("The file was saved!");
+//     });
+
+//     res.status(200).json({
+//         message: "Height Stored successfully"
+//     })
+// });
+
+// app.get('/getHeight', function (req, res) {
+//     fs.readFile('./server/height.txt','utf8', function (err, data) {
+//         if (err) {
+//             return console.log(err)
+//         }
+//         const content = data;
+//         res.status(200).json({
+//             data: content
+//         })
+//     });
+// });
+
+// app.get('/clear', function (req, res) {
+//     fs.writeFile("./server/data.txt", "", function(err) {
+//         if(err) {
+//             return console.log(err);
+//         }
+//         console.log("The file was saved!");
+//     });
+
+//     fs.writeFile("./server/height.txt", "700", function(err) {
+//         if(err) {
+//             return console.log(err);
+//         }
+//         console.log("The file was saved!");
+//     });
+
+//     res.status(200).render('index', {admin: false});
+// });
+
+io.sockets.on('connection', function (socket) {
+     socket.on('mousemove', function(data) {
+        socket.id = data.id;
+        io.emit('moving', data);
+     });
+     socket.on('disconnect', function() {
+         socket.broadcast.emit('left', socket.id);
+     });
+});
+
+server.listen(process.env.PORT, () => {
+    console.log("server runs on port " + process.env.PORT);
+})
---
+++ public/js/index.js
@@ -0,0 +1,232 @@
+
+$(function(){
+    var canvas = $('#board');
+    var ctx = canvas[0].getContext('2d');
+    var line_color = $('#line_color');
+    var line_width = $('#line_width');
+    var save_btn = $('#save_btn');
+    var inc_btn = $('#increase');
+    var scroll_options = $('#scroll_options');
+    var draw_mode = $('#draw_mode')
+
+	// Generate an unique ID
+	var id = Math.round($.now()*Math.random());
+
+    // the variables for the user
+    var cColor = '#000'
+    var cLineWidth = 1;
+	var cDrawing = false;
+    var cLastEmit = $.now();
+    var scroll = 'draw';
+    var mode = 'pen';
+
+	var clients = {};
+    var cursors = {};
+    var touch_control;
+
+    // fetch('/getHeight')
+    // .then(res => res.json())
+    // .then(json => {
+    //     ctx.canvas.height = json.data;
+    // })
+    
+    // fetch('/load')
+    // .then(res => res.json())
+    // .then(json => {
+    //     image = new Image;
+    //     image.src = json.data;
+    //     image.onload = function(){
+    //         ctx.drawImage(image, 0, 0)
+    //     }
+    // })
+
+	var socket = io.connect();
+
+	// socket.on('moving', function (data) {
+    //     // not current user and new? create a cursor
+	// 	if(id !== data.id && !(data.id in clients)){
+	// 		cursors[data.id] = jQuery('.cursor').appendTo('#cursors');
+
+    //         // Move the mouse pointer
+    //         cursors[data.id].css({
+    //             'left' : data.x,
+    //             'top' : data.y
+    //         });
+	// 	}
+
+	// 	// Is the user drawing?
+	// 	if(data.drawing && clients[data.id]){
+    //         draw(clients[data.id], data, data.color, data.lineWidth);
+	// 	}
+
+	// 	// Save the last data state
+    //     clients[data.id] = data;
+    // });
+    
+    function onMoving(data) {
+        if(id !== data.id && !(data.id in clients)){
+			cursors[data.id] = jQuery('.cursor').appendTo('#cursors');
+
+            // Move the mouse pointer
+            cursors[data.id].css({
+                'left' : data.x,
+                'top' : data.y
+            });
+		}
+
+		// Is the user drawing?
+		if(data.drawing && clients[data.id]){
+            draw(clients[data.id], data, data.color, data.lineWidth);
+		}
+
+		// Save the last data state
+        clients[data.id] = data;
+    }
+
+    socket.on('left', function (id) {
+        cursors[id].remove();
+    });
+
+    // set cDrawing to true
+	canvas.on('mousedown',function(e){
+		e.preventDefault();
+        cDrawing = true;
+    });
+    
+    canvas.on('touchstart',function(e){
+        if(scroll === 'draw') {
+            e.preventDefault();       
+        }
+        cDrawing = true;
+        touch_control = 1;
+	});
+
+    // not drawing anymore
+	canvas.on('mouseup mouseleave',function(){
+        cDrawing = false;
+    });
+    
+    canvas.on('touchend',function(){
+        cDrawing = false;
+	});
+
+
+    // send the current state to the server if the time difference from last emit is big enough
+	canvas.on('mousemove',function(e){
+        var cPos = {x: e.pageX-286, y: e.pageY-51 + $('#canvas_container').scrollTop()};
+		if($.now() - cLastEmit > 30){
+			onMoving({
+				'x': cPos.x,
+				'y': cPos.y,
+				'drawing': cDrawing,
+				'id': id,
+                'color': cColor,
+                'lineWidth': cLineWidth
+			});
+			cLastEmit = $.now();
+		}
+    });
+    
+    canvas.on('touchmove',function(e){
+        if (touch_control === 1 || scroll === 'scroll'){
+            cDrawing = false;
+        } else {
+            cDrawing = true;
+        }
+
+        touch = undefined;
+        if (e.originalEvent.touches)
+          touch = e.originalEvent.touches[0];
+
+        var cPos = {x: touch.pageX-20, y: touch.pageY-70 + $('#canvas_container').scrollTop()};
+		if($.now() - cLastEmit > 30){
+			onMoving({
+				'x': cPos.x,
+				'y': cPos.y,
+				'drawing': cDrawing,
+				'id': id,
+                'color': cColor,
+                'lineWidth': cLineWidth
+			});
+			cLastEmit = $.now();
+        }
+        touch_control += 1;
+	});
+
+
+	function draw(from, to, color, w){
+        ctx.strokeStyle = color;
+        ctx.lineWidth = w;
+        if(mode === 'pen') {
+            ctx.globalCompositeOperation='source-over';
+            ctx.lineWidth = w;
+        } else {
+            ctx.globalCompositeOperation='destination-out';
+            ctx.lineWidth = 40;
+        }
+        ctx.beginPath();
+		ctx.moveTo(from.x, from.y);
+		ctx.lineTo(to.x, to.y);
+		ctx.stroke();
+        ctx.closePath();
+    }
+    
+    //color
+    line_color.on('input', function() {
+        cColor = line_color.val();
+    });
+
+    //linewidth
+    line_width.on('input', function() {
+        cLineWidth = line_width.val();
+    });
+
+    draw_mode.on('input', function() {
+        mode = draw_mode.val();
+    });
+
+    // //save
+    // save_btn.on('click', function() {
+    //     var saveData = canvas[0].toDataURL();
+
+    //     fetch('/save', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //           },
+    //         body: JSON.stringify({data: saveData})
+    //     })
+    //     .then(response => response.json())
+    //     .then(data => console.log(data))
+    //     alert('Saved successfully');
+    // })
+
+    inc_btn.on('click', function() {
+        ctx.canvas.height += 1200;
+
+        fetch('/saveHeight', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify({data: ctx.canvas.height})
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+
+        fetch('/load')
+        .then(res => res.json())
+        .then(json => {
+            image = new Image;
+            image.src = json.data;
+            image.onload = function(){
+                ctx.drawImage(image, 0, 0)
+            }
+        })
+    });
+
+    scroll_options.change(function() {
+        scroll = $('input[name = scroll]:checked').val();
+    })
+
+});
